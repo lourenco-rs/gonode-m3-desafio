17e64854837e894a38085aab760d396ef5a16b04
Rodrigo Lourenço <lourenco.rs@gmail.com>
2018-12-18 18:50:12 -0200

    Aula 6 - Criptografia de senha

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -12,10 +13,28 @@ const UserSchema = new mongoose.Schema({
     // o email será salvo em minúsculas, mesmo que tenha sido informado em maiúscula
     lowercase: true
   },
+  password: {
+    type: String,
+    required: true
+  },
   created_at: {
     type: Date,
     default: Date.now
   }
+})
+
+UserSchema.pre('save', async function (next) {
+  /** usa-se a declaração da function na forma clássica, pois o mongoose
+   * da no this a instância com todos os dados do usuário.
+   */
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  /**
+   * 8 - representa a força da senha
+   */
+  this.password = await bcrypt.hash(this.password, 8)
 })
 
 module.exports = mongoose.model('User', UserSchema)
