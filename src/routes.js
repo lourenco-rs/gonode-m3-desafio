dea5078d65dd4c70a0ef58ec2c33778770c24347
Rodrigo Lourenço <lourenco.rs@gmail.com>
2018-12-19 17:57:04 -0200

    Aula 12 - CRUD de Ad

--- src/routes.js
+++ src/routes.js
@@ -8,9 +8,15 @@ const authMiddleware = require('./app/middlewares/auth')
 const controllers = require('./app/controllers')
 
 routes.post('/users', controllers.UserController.store)
-
 routes.post('/sessions', controllers.SessionController.store)
 
-routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
+// aplica o middleware para as rotas a partir desse ponto
+routes.use(authMiddleware)
+
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+routes.put('/ads/:id', controllers.AdController.update)
+routes.delete('/ads/:id', controllers.AdController.destroy)
 
 module.exports = routes
