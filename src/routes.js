a992cec27cbba5b7b14d8289e51eef82825f8f1f
Rodrigo Lourenço <lourenco.rs@gmail.com>
2018-12-19 16:56:17 -0200

    Aula 10 - Utilizando require-dir

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,15 @@ const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+// importa o index.js da pasta controllers
+const controllers = require('./app/controllers')
 
-routes.post('/users', UserController.store)
+// const UserController = require('./app/controllers/UserController')
+// const SessionController = require('./app/controllers/SessionController')
 
-routes.post('/sessions', SessionController.store)
+routes.post('/users', controllers.UserController.store)
+
+routes.post('/sessions', controllers.SessionController.store)
 
 routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
 
