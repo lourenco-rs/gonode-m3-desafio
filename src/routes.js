11f01d4f951fbc91f2aa7adc22f8b55980ae1ce4
Rodrigo Lourenço <lourenco.rs@gmail.com>
2018-12-19 17:06:30 -0200

    Aula 11 - Schema de Ad

--- src/routes.js
+++ src/routes.js
@@ -7,9 +7,6 @@ const authMiddleware = require('./app/middlewares/auth')
 // importa o index.js da pasta controllers
 const controllers = require('./app/controllers')
 
-// const UserController = require('./app/controllers/UserController')
-// const SessionController = require('./app/controllers/SessionController')
-
 routes.post('/users', controllers.UserController.store)
 
 routes.post('/sessions', controllers.SessionController.store)
