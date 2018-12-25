522ed1e277633ae2ccfc4e3006b4aee5618f85ba
Rodrigo Lourenço <lourenco.rs@gmail.com>
2018-12-25 07:58:27 -0200

    Aula 19 - Validacao

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const validate = require('express-validation')
 
 const routes = express.Router()
 
@@ -6,9 +7,18 @@ const authMiddleware = require('./app/middlewares/auth')
 
 // importa o index.js da pasta controllers
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 
 // aplica o middleware para as rotas a partir desse ponto
 routes.use(authMiddleware)
@@ -18,13 +28,17 @@ routes.use(authMiddleware)
  */
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
 
 /**
  * Purchases
  */
-routes.post('/purchases', controllers.PurchaseController.store)
+routes.post(
+  '/purchases',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 
 module.exports = routes
